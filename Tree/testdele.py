dd6e1d17649d59d18c7d143d9ebada250294081a
Poowadol Rungtabnapa <51953419+PoowadolRungtabnapa@users.noreply.github.com>
2020-02-08 11:13:08 +0700

    .

--- Tree/testdele.py
+++ Tree/testdele.py
@@ -1,3 +1,4 @@
+index = 0
 class Node :
 
     def __init__(self, data):
@@ -22,38 +23,57 @@ class Node :
         else :
             self.data = data
     
+    def DelNode (self, dele) :
+        if self.right.left is None :
+            self.right.DelNode(dele)
+        else :
+            self.data = self.right.left.data
+            self.right.left.data = ""
+
+    def LoopLeft (self) :
+        if self.left is None and self.right is None :
+            data = self.data
+            self.data = ""
+            return data
+        else :
+            if self.left is not None :
+                self.left.LoopLeft()
+            else :
+                self.right.LoopLeft()
+
     def Deletion(self, dele) :
-        if dele == self.data : # ไม่มีตัวเชื่อม
-            indexl = 0
-            indexr = 0
-            if self.left is None :
-                indexl = 1
-            if self.right is None :
-                indexr = 1
-            if indexl == 1 and indexr == 1 :
-                self.data = None
-            else : # มีตัวเชื่อม
-                indexl = 0
-                indexr = 0
-                if self.left is not None :
-                    indexl = 1
-                if self.right is not None :
-                    indexr = 1
-                if self.left == 1 or self.right == 1 :
-                    self.data = self.left.data
-                    self.left.data = None
-        elif dele != self.data :
-            if dele < self.data :
-                if self.left is None :
-                    pass
-                return self.left.Deletion(dele)
-            elif dele > self.data :
-                if self.right is None :
-                    pass
-                return self.right.Deletion(dele)
-        
-        # ตัวบนสุด
-        
+        global index
+        number = self.data
+        if dele == root.data :
+           self.DelNode(dele)
+        else :
+            if dele == self.data and index == 1 :
+                if self.left is None and self.right is None :
+                    self.data = ""
+                else :
+                    if self.left is not None :
+                        self.data = self.left.LoopLeft()
+                    else :
+                        self.data = self.right.LoopLeft()
+                        print(self.data)
+            if dele == self.data and index == 2 :
+                if self.left is None and self.right is None :
+                    self.data = ""
+                else :
+                    if self.left is not None :
+                        self.data = self.left.LoopLeft()
+                    else :
+                        self.data = self.right.LoopLeft()
+            if self.left is not None and self.data == number :
+                if dele < self.data :
+                    if index == 0 :
+                        index = 1
+                    self.left.Deletion(dele)
+            if self.right is not None and self.data == number :
+                if dele > self.data :
+                    if index == 0 :
+                        index = 2
+                    self.right.Deletion(dele)
 
     def PrintTree(self) :
         if self.left :
@@ -66,6 +86,7 @@ root = Node(100)
 root.insert(50)
 root.insert(30)
 root.insert(60)
+root.insert(35)
 root.insert(150)
 root.insert(120)
 
